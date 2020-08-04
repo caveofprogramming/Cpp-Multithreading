e53db22b4e3f474fac846968e7a2ada1bcb81a69
John Purcell <john.purcell@caveofprogramming.com>
2020-08-04 12:43:39 +0200

    Created

--- 110 Promises and Exceptions.cpp
+++ 110 Promises and Exceptions.cpp
@@ -3,38 +3,57 @@
 #include <iomanip>
 #include <thread>
 #include <future>
+#include <exception>
 
 using namespace std;
 
-double calculate_pi(int terms) 
+double calculate_pi(int terms)
 {
     double sum = 0.0;
 
-    for(int i = 0; i < terms; i++)
+    if (terms < 1)
+    {
+        throw runtime_error("Terms cannot be less than 1");
+    }
+
+    for (int i = 0; i < terms; i++)
     {
         int sign = pow(-1, i);
-        double term = 1.0/(i * 2 + 1);
+        double term = 1.0 / (i * 2 + 1);
         sum += sign * term;
     }
 
     return sum * 4;
 }
 
-int main() 
+int main()
 {
     promise<double> promise;
 
-    auto do_calculation = [&](int terms){
-        auto result = calculate_pi(terms);
-
-        promise.set_value(result);
+    auto do_calculation = [&](int terms) {
+        try
+        {
+            auto result = calculate_pi(terms);
+            promise.set_value(result);
+        }
+        catch (...)
+        {
+            promise.set_exception(current_exception());
+        }
     };
 
-    thread t1(do_calculation, 1E8);
+    thread t1(do_calculation, 1E6);
 
     future<double> future = promise.get_future();
-    
-    cout << setprecision(15) << future.get() << endl;
+
+    try
+    {
+        cout << setprecision(15) << future.get() << endl;
+    }
+    catch (const exception &e)
+    {
+        cout << e.what() << endl;
+    }
 
     t1.join();
     return 0;
