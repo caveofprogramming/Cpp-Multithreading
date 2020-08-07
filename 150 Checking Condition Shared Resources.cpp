e7b578c539307dd7e13daeaef3d05b0817054cfc
John Purcell <john.purcell@caveofprogramming.com>
2020-08-07 17:50:05 +0200

    Created

--- 150 Checking Condition Shared Resources.cpp
+++ 150 Checking Condition Shared Resources.cpp
@@ -11,21 +11,25 @@ int main()
     mutex mtx;
     bool ready = false;
 
-    thread t1([&](){
+    thread t1([&]() {
         this_thread::sleep_for(chrono::milliseconds(2000));
+
+        cout << "t1 acquiring lock" << endl;
         unique_lock<mutex> lock(mtx);
+        cout << "t1 acquired lock" << endl;
         ready = true;
         lock.unlock();
+        cout << "t1 released lock; notifying" << endl;
         condition.notify_one();
     });
 
+    cout << "main thread acquiring lock" << endl;
     unique_lock<mutex> lock(mtx);
 
-    while(!ready)
-    {
-        condition.wait(lock);
-    }
+    cout << "main thread acquired lock; waiting" << endl;
+    condition.wait(lock, [&](){ return ready; });
 
+    cout << "main thread finished waiting" << endl;
     cout << "ready " << ready << endl;
 
     t1.join();
