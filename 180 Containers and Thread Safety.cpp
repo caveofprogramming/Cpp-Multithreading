04e5e1f6c9c580fa4a6def6e4246fafcdf433e59
John Purcell <john.purcell@caveofprogramming.com>
2020-08-08 12:06:22 +0200

    Created

--- 180 Containers and Thread Safety.cpp
+++ 180 Containers and Thread Safety.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
 #include <thread>
+#include <queue>
 
 using namespace std;
 
 template<typename E>
 class blocking_queue
 {
+private:
+    int _max_size;
+    queue<E> _queue;
+
+
 public:
-    void push(E e)
+    blocking_queue(int max_size): _max_size(max_size)
     {
-        cout << "push" << endl;
+
     }
 
-    void pop()
+    void push(E e)
     {
-        cout << "pop" << endl;
+        _queue.push(e);
+    }
+
+    E pop()
+    {
+        E item = _queue.front();
+        _queue.pop();
+        return item;
     }
 };
 
 int main()
 {
-    blocking_queue<int> qu;
+    blocking_queue<int> qu(5);
 
-    thread t1(&blocking_queue<int>::push, &qu, 7);
-    thread t2(&blocking_queue<int>::pop, &qu);
+    thread t1([&](){
+        for(int i = 0; i < 10; i++)
+        {
+            qu.push(i);
+        }
+    });
+
+    thread t2([&](){
+        for(int i = 0; i < 10; i++)
+        {
+            auto item = qu.pop();
+            cout << "consumed " << item << endl;
+        }
+    });
 
     t1.join();
     t2.join();
