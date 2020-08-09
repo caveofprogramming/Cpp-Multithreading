12cf711f557f77c436892e6d5c21daf4affc59b0
John Purcell <john.purcell@caveofprogramming.com>
2020-08-09 20:08:58 +0200

    Created

--- 210 Async.cpp
+++ 210 Async.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <future>
+#include <chrono>
 
 using namespace std;
 
+int work(int id)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "running " << id << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+
+    return id * 7;
+}
 
 int main()
 {
-   
+    future<int> f1 = async(launch::async, work, 0);
+    future<int> f2 = async(launch::async, work, 1);
+
+    cout << f1.get() << endl;
+    cout << f2.get() << endl;
     return 0;
 }
